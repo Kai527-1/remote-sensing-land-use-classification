46afb8ca1af70ea16d2e6129adae2c9735725a4f
Kai <khe89857@gmail.com>
2026-04-19 09:46:39 +1000

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,1 +1,54 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
 
+
+def get_transforms(img_size=64):
+    train_transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    return train_transform, test_transform
+
+
+def get_dataloaders(data_dir, batch_size=32, img_size=64, train_ratio=0.8, val_ratio=0.1):
+    train_transform, test_transform = get_transforms(img_size)
+
+    full_dataset = datasets.ImageFolder(root=data_dir)
+    class_names = full_dataset.classes
+
+    total_size = len(full_dataset)
+    train_size = int(train_ratio * total_size)
+    val_size = int(val_ratio * total_size)
+    test_size = total_size - train_size - val_size
+
+    train_dataset, val_dataset, test_dataset = random_split(
+        full_dataset,
+        [train_size, val_size, test_size]
+    )
+
+    train_dataset.dataset.transform = train_transform
+    val_dataset.dataset.transform = test_transform
+    test_dataset.dataset.transform = test_transform
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader, class_names
